622640b79ef203663594769f5f3948135f321415
ChakradharA2003 <chakradharchakri018@gmail.com>
2025-04-10 13:42:29 +0530

    debug

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import speedtest
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,5 @@ def speed_check():
         return jsonify({'download': 'Error', 'upload': 'Error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
